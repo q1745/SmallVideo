187f092ab8b1d6defd84a654191f6d69ac9bbecb
zhang <3195152451@qq.com>
2021-10-20 09:18:44 +0800

    SP存储

---
+++ Utils/src/main/java/com/example/utils/BaseConstant.kt
@@ -0,0 +1,10 @@
+package com.example.common
+
+class BaseConstant {
+    companion object{
+        //SP存储表名
+        const val SP_TABLENAME : String = "electricity"
+        //SP存储登录状态Key
+        const val SP_ISLOGIN : String = "isLoginisTrue"
+    }
+}
